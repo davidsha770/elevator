eb2444dda11cba358ba4a74b2be598df96a0f359
davidsha770 <davidsha207@gmail.com>
2024-04-30 14:55:23 +0300

    add builders

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 from building import Building
 
 # Initialize Pygame
@@ -8,12 +9,18 @@ pygame.init()
 
 # Set up the display
 height_screen = 700
-width_screen = 400
+width_screen = 1000
+height_floor = 30
+width_floor = 100
 screen = pygame.display.set_mode((width_screen, height_screen))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Building Floors")
 
-building = Building(22, 3, 100, 30, height_screen)
+street = []
+
+for i in range(3):
+    building = Building(10 + i * width_floor * 3 , random.randint(10, 20), 3, width_floor, height_floor, height_screen)
+    street.append(building)
 
 # Main loop
 running = True
@@ -25,12 +32,14 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # Check if left mouse button is pressed
             if event.button == 1:
-                building.process_floor_click(pygame.mouse.get_pos())
+                for building in street:
+                    building.process_floor_click(pygame.mouse.get_pos())
 
     # Draw the floors
     screen.fill((255, 255, 255))  # Fill the screen with white
-    building.draw(screen)
-    building.process_elevator_movement()
+    for building in street:
+        building.draw(screen)
+        building.process_elevator_movement()
 
     # Update the display
     pygame.display.flip()
