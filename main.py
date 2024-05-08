217e02689700be092740c60db4676615a4aaf796
davidsha770 <davidsha207@gmail.com>
2024-05-08 13:42:53 +0300

    clean main

--- main.py
+++ main.py
@@ -3,27 +3,20 @@ import sys
 from configuration import load_config
 from street import Street
 
-def initialize_pygame(width_screen, height_screen):
+def main():
+    height_screen, width_screen, buildings_info = load_config('configuration.txt')
     pygame.init()
     screen = pygame.display.set_mode((width_screen, height_screen))
     pygame.display.set_caption("Building Floors")
-    return screen
-
-def draw_screen(screen, street):
-    screen.fill((255, 255, 255))  # Fill the screen with white
-    street.draw(screen)
-    pygame.display.flip()
-
-def main():
-    height_screen, width_screen, buildings_info = load_config('configuration.txt')
-    screen = initialize_pygame(width_screen, height_screen)
     street = Street(buildings_info, height_screen, width_screen)
     clock = pygame.time.Clock()
 
     running = True
     while running:
         running = street.handle_events()
-        draw_screen(screen, street)
+        screen.fill((255, 255, 255))  # Fill the screen with white
+        street.draw(screen)
+        pygame.display.flip()
         clock.tick(60)
 
     pygame.quit()
